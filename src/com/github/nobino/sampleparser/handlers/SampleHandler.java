10d40b973985a11b9f97e771ca329cdfecb3df17
nobino <nobino_github@outlook.com>
2013-08-01 00:08:17 +0900

    print top level types of the first CompilationUnit in selections.

--- src/com/github/nobino/sampleparser/handlers/SampleHandler.java
+++ src/com/github/nobino/sampleparser/handlers/SampleHandler.java
@@ -1,11 +1,22 @@
 package com.github.nobino.sampleparser.handlers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeSelection;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -24,11 +35,61 @@ public class SampleHandler extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		ITreeSelection tree;
+		
+		if (selection instanceof ITreeSelection) {
+			tree = (ITreeSelection) selection;
+		} else {
+			System.out.println("selection does not implement ITreeSelection.");
+			return null;
+		}
+		
+		List<ICompilationUnit> compilationUnits = getCompilationUnits(tree);
+		
+		if(compilationUnits.isEmpty()) {
+			System.out.println("tree has no CompilationUnit.");
+			return null;
+		}
+
+		ICompilationUnit compilationUnit = compilationUnits.get(0);
+		
+		List<IType> types = getTypes(compilationUnit); 
+
+		for(IType type : types) {
+			System.out.println(type.toString());
+		}
+		
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(
 				window.getShell(),
 				"Sampleparser",
-				"Hello, Eclipse world");
+				selection.toString());
 		return null;
+	}
+	
+	private List<ICompilationUnit> getCompilationUnits(ITreeSelection tree) {
+		List<ICompilationUnit> results = new ArrayList<>();
+		
+		@SuppressWarnings("rawtypes")
+		Iterator iterator = tree.iterator();
+		while(iterator.hasNext()) {
+			Object next = iterator.next();
+			if (next instanceof ICompilationUnit) {
+				results.add((ICompilationUnit) next);
+			}
+		}
+		
+		return results;
+	}
+	
+	private List<IType> getTypes(ICompilationUnit compilationUnit) {
+		try {
+			return Arrays.asList(compilationUnit.getTypes());
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Collections.emptyList();
 	}
 }
